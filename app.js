350379b6318ddb1d8df09f81a2163e3419e91daf
DavidP001 <pp876314955@hotmail.com>
2019-01-30 14:25:45 +0800

    add login navbar
    增加login功能的UI和js逻辑，优化search UI，增加导航栏

--- app.js
+++ app.js
@@ -1,2 +1,35 @@
 //app.js
-App({})
+App({
+  globalData: {
+    personnelno: null
+  },
+  onLaunch: function() {
+    this.quicklogin()
+  },
+  checkLoginStatus: function() {
+    let{personnelno} = this.globalData
+    if(personnelno != null) {
+      return true
+    }
+    else {
+      isLogin = this.quickLogin()
+      return isLogin
+    }
+  },
+  quickLogin: function() {
+    try {
+      const value = wx.getStorageSync('session_key')
+      if(value) {
+        wx.request({
+          url: 'https://davidp.top/api/quicklogin',
+          method: 'GET',
+          
+
+        })
+        return true
+      }
+    } catch (e) {
+      return false
+    }
+  }
+})
---
+++ pages/user/user.js
@@ -0,0 +1,10 @@
+// pages/user/user.js
+Page({
+  data: {
+  },
+  logintap: function() {
+    wx.navigateTo({
+      url: '../login/login'
+    })
+  }
+})
